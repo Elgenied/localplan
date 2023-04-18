1abfdd300c312238854442496188ff8d50203331
Elgenied Elqurashi <e.elqurashi@surrey.ac.uk>
2023-04-18 01:46:49 +0100

    added ipyleaflet functions

--- localplan/localplan.py
+++ localplan/localplan.py
@@ -1,38 +1,74 @@
 """Main module."""
 import random
 import string
+import ipyleaflet
 
-def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
-    """generate_random_string
+class Map(ipyleaflet.Map):
+    """Map class
 
     Args:
-        length (int, optional): _description_. Defaults to 10.
-        upper (bool, optional): _description_. Defaults to False.
-        digits (bool, optional): _description_. Defaults to False.
-        punctuation (bool, optional): _description_. Defaults to False.
-
-    Returns:
-        _type_: _description_
+        ipyleaflet (_type_): _description_.
     """    
-    letters = string.ascii_lowercase
-    if upper:
-        letters += string.ascii_uppercase
-    if digits:
-        letters += string.digits
-    if punctuation:
-        letters += string.punctuation
+    def __init__(self, center, zoom, **kwargs):
+       
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"] = True
+        super().__init__(center=center, zoom=zoom, **kwargs)
+   
+    def add_Search_control(self, position='topleft', **kwargs):
+        """add search control to map
+
+        Args:
+            position (str, optional): _description_. Defaults to 'topleft'.
+        """        
+        if "url" not in kwargs:
+            kwargs["url"] = 'https://nominatim.openstreetmap.org/search?format=json&q={s}'
+        search_control = ipyleaflet.SearchControl(position=position, **kwargs)
+        self.add_control(search_control)
     
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
+    def add_draw_control(self, **kwargs):
+        """add draw control to map
+        """        
+        draw_control = ipyleaflet.DrawControl(**kwargs)
+        draw_control.polyline =  {
+            "shapeOptions": {
+                "color": "#6bc2e5",
+                "weight": 8,
+                "opacity": 1.0
+            }
+        }
+        draw_control.polygon = {
+            "shapeOptions": {
+                "fillColor": "#6be5c3",
+                "color": "#6be5c3",
+                "fillOpacity": 1.0
+            },
+            "drawError": {
+                "color": "#dd253b",
+                "message": "Oups!"
+            },
+            "allowIntersection": False
+        }
+        draw_control.circle = {
+            "shapeOptions": {
+                "fillColor": "#efed69",
+                "color": "#efed69",
+                "fillOpacity": 1.0
+            }
+        }
+        draw_control.rectangle = {
+            "shapeOptions": {
+                "fillColor": "#fca45d",
+                "color": "#fca45d",
+                "fillOpacity": 1.0
+            }
+        }
+        
+        
+        
+        
+        self.add_control(draw_control)
 
-def generate_lucky_number(length=1):
-    """_generate_lucky_number
+       
 
-    Args:
-        length (int, optional): _description_. Defaults to 1.
-
-    Returns:
-        _str_: rerurns a string of length 1
-    """    
-    result_str = ''.join(random.choice(string.digits) for i in range(length))
-    return result_str
+    
